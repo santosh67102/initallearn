8b60c55461829ee611a84094164db172544abd2f
A634025 <A634025@V720711.dmn1.fmr.com>
2018-09-07 15:55:48 -0400

    correct then to and

--- greenfield1/src/test/java/stepDefinition/Dashboard.java
+++ greenfield1/src/test/java/stepDefinition/Dashboard.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.paulhammant.ngwebdriver.ByAngularBinding.FindBy;
-
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
